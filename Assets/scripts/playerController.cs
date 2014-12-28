c9f39bc360b77f65d2131216e1e33b6c5bb8c89a
Yiqing Liang <liangyiqing.in.china@gmail.com>
2014-12-28 02:15:43 +0200

    first upload

---
+++ Assets/scripts/playerController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class playerController : MonoBehaviour {
+
+	public float speed;
+	private int count;
+	public Text countText;
+	public Text winText;
+
+	void Start (){
+		count = 0;
+		SetCountText ();
+		winText.text = "";
+	}
+
+	void SetCountText(){
+		countText.text = "Count: " + count.ToString();
+		if (count >= 7) {
+			winText.text = "You win";
+		}
+	}
+
+	void FixedUpdate() {
+		float moveVertical = Input.GetAxis ("Vertical");
+		float moveHorizontal = Input.GetAxis ("Horizontal") ;
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		// change the speed of ball
+		rigidbody.AddForce (movement*speed*Time.deltaTime);
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.tag == "pickup"){
+			other.gameObject.SetActive(false);
+			count = count + 1;
+			SetCountText ();
+		}
+	}
+}
